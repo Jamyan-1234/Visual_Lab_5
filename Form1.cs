518747e48bc066397bf98ec608070417b88eb8ca
Jamyan <jamyan2470@gmail.com>
2024-03-10 23:56:26 +0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace lab5
+{
+    public partial class JuiceBarForm : Form
+    {
+        private decimal itemPriceDecimal, totalOrderDecimal, totalSalesDecimal;
+        private int drinksInteger, ordersInteger;
+        public JuiceBarForm()
+        {
+            InitializeComponent();
+        }
+
+        private void JuiceBarForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addToOrderButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void orderCompleteButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
