a23614113c19b02a95f63ac030e379d11504fa75
Jamyan <jamyan2470@gmail.com>
2024-03-13 18:04:27 +0800

    hmm

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace lab5
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
+            // To customize application configuration such as set high DPI segttings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new JuiceBarForm());
